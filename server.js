3aff8ee6bc550465cf7fb9e3a95c931dea52ea9f
Adam Gunther <adamgunther1@gmail.com>
2017-09-06 19:33:17 -0600

    created user model to save user to database and begins setting up passport for google oauth

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var session = require('express-session');
 // mongodb configuration =================
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_mzb7zzrt:gg91gpi6sd9rgah2dpa81kmp05@ds119044.mlab.com:19044/heroku_mzb7zzrt')
 
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up express application
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
@@ -23,10 +24,10 @@ app.use(bodyParser.urlencoded({'extended':'true'}));            // parse applica
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-app.use(cookieParser());                                        // read cookies (nneded for auth)
+app.use(cookieParser());                                        // read cookies (needed for auth)
 
 
-// required for passport
+// // required for passport
 app.use(session({ secret: 'passportsletyoutravelaroundmoreoftherockwecallearth' }));  // session secret
 app.use(passport.initialize());
 app.use(passport.session());                                    // persistent login sessions
@@ -35,6 +36,7 @@ app.use(flash());                                               // use connnect-
 
 // load the routes
 require('./app/routes')(app, passport);                         // load our routes and pass in our app and fully configured passport
+// require('./app/routes')(app);                         // load our routes and pass in our app
 
 
 // listen (start app with node server.js) ======================================
