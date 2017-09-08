9a6470963eeaef7db30e9bc6602ac754be35dc00
Adam Gunther <adamgunther1@gmail.com>
2017-09-08 14:32:41 -0600

    changes method from findOrCreateback to findOne since it's not define

--- config/passport.js
+++ config/passport.js
@@ -24,8 +24,7 @@ module.exports = function (passport) {
   function(token, refreshToken, profile, done) {
     process.nextTick(function() {
 
-      User.findOrCreate({ 'google.id' : profile.id }, function(err, user) {
-        console.log('ehhhhh')
+      User.findOne({ 'google.id' : profile.id }, function(err, user) {
         if (err)
           return done(err);
         if (user) {
