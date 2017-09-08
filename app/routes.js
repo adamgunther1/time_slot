edfe3d4597c64fb95249c45325bdde6b1310bacd
Adam Gunther <adamgunther1@gmail.com>
2017-09-08 13:53:44 -0600

    adds target _self to auth/google link to not show blank layout and instead direct to google oauth

--- app/routes.js
+++ app/routes.js
@@ -63,7 +63,6 @@ module.exports = function(app, passport) {
     // });
 
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-    // app.get('/auth/google', passport.authenticate('google'));
 
     app.get('/auth/google/callback',
         passport.authenticate('google', {
