8caa8a06de6a34a7bcd8e7a3ec8fd902ebdd1b26
Adam Gunther <adamgunther1@gmail.com>
2017-09-11 17:44:05 -0600

    adds auth parameters to get calendar list api request

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,11 +3,6 @@ angular.module('mainCtrl', [])
   .controller('mainController', function($scope, $http, Todos) {
     $scope.formData = {};
 
-    Todos.getCalendar()
-        .success(function(calendar){
-            console.log(calendar)
-        })
-
     Todos.getUser()
         .success(function(user){
             if (user !== '0') {
