8ba9169330fd518e4d2f49ca61e763733733dba7
Adam Gunther <adamgunther1@gmail.com>
2017-09-12 22:09:36 -0600

    saving multiple database entries at a time trying to narrow down the breaking point

--- app/routes.js
+++ app/routes.js
@@ -25,10 +25,10 @@ module.exports = function(app, passport) {
             // user = req.body;
             user.calendar.kind = req.body.calendar.kind;
             user.calendar.etag = req.body.calendar.etag;
-            user.calendar.summary = calendar.summary;
-            user.calendar.timeZone = calendar.timeZone;
-            user.calendar.accessRole = calendar.accessRole;
-            user.calendar.nextSyncToken = calendar.nextSyncToken;
+            user.calendar.summary = req.body.calendar.summary;
+            user.calendar.timeZone = req.body.calendar.timeZone;
+            user.calendar.accessRole = req.body.calendar.accessRole;
+            user.calendar.nextSyncToken = req.body.calendar.nextSyncToken;
             // console.log('NEW USER')
             // console.log(user)
             // user.save();
